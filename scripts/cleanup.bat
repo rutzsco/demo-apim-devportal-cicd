f0e24bb93891649bb9816efd058ce205dde4867e
Scott Rutz <scrutz@microsoft.com>
2020-08-27 12:08:48 -0500

    wip

---
+++ scripts/cleanup.bat
@@ -0,0 +1,7 @@
+@REM Delete the content of an API Management portal - incl. pages, layouts, configuration, media files, etc.
+
+set management_endpoint="< service name >.management.azure-api.net"
+set access_token="SharedAccessSignature ..."
+set storage_connection_string="DefaultEndpointsProtocol=..."
+
+node ./cleanup %management_endpoint% %access_token% %storage_connection_string%
---
+++ scripts/generate.bat
@@ -0,0 +1,11 @@
+@REM Generate and provision default content of an API Management portal - incl. pages, layouts, configuration, media files, etc.
+
+set management_endpoint="< service name >.management.azure-api.net"
+set access_token="SharedAccessSignature ..."
+set storage_connection_string="DefaultEndpointsProtocol=..."
+set container="content"
+set data_file="./data.json"
+set media_folder="./media"
+
+node ./generate %management_endpoint% %access_token% %data_file%
+node ./upload %storage_connection_string% %media_folder% %container%
